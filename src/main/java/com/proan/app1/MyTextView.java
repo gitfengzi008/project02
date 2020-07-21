9e0b70e4332ace6f78226291fb66a390a9be1770
gitfengzi008 <43361448+gitfengzi008@users.noreply.github.com>
2020-07-21 17:52:31 +0800

    Add files via upload

---
+++ src/main/java/com/proan/app1/MyTextView.java
@@ -0,0 +1,56 @@
+package com.proan.app1;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class MyTextView extends AppCompatTextView {
+    private int mViewWidth = 0;
+    private Paint mPaint;
+    private LinearGradient mLinearGradient;
+    private Matrix mGradientMatrix;
+    private int mTranslate;
+
+    public MyTextView(Context context) {
+        super(context);
+    }
+
+    public MyTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mViewWidth == 0) {
+            mViewWidth = getMeasuredWidth();
+            if (mViewWidth > 0) {
+                mPaint = getPaint();
+                mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0, new int[]{Color.RED, 0xffffffff, Color.RED}, null, Shader.TileMode.CLAMP);
+                mPaint.setShader(mLinearGradient);
+                mGradientMatrix = new Matrix();
+            }
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (mGradientMatrix != null) {
+            mTranslate += mViewWidth / 5;
+            if (mTranslate > 2 * mViewWidth) {
+                mTranslate = -mViewWidth;
+            }
+            mGradientMatrix.setTranslate(mTranslate, 0);
+            mLinearGradient.setLocalMatrix(mGradientMatrix);
+            postInvalidateDelayed(50);
+        }
+    }
+}
